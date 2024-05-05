e895005e1892456272a4c2efde49dfa2b737fbef
AyaJuanD <134539283+AyaJuanD@users.noreply.github.com>
2024-05-04 21:02:19 -0500

    pruebasURL

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
-require_once 'src/config/config.php';
-$currenPageUrl = $_SERVER['REQUEST_URI'];
-echo $currenPageUrl;
-$ruta = empty($_GET['url']) ? 'principal/index' : $_GET['url'];
+    require_once 'src/config/config.php';
+    //capturar la uRL actual
+    $currenPageUrl = $_SERVER['REQUEST_URI'];
+    //verificar si existe la ruta admin
+    $isAdmin = strpos($currenPageUrl, '/' . ADMIN) !== false;
+    //comprobar si exite GeT para crear URLS amigables
+    $ruta = empty($_GET['url']) ? 'principal/index' : $_GET['url'];
+    //crear un arrya a partir de la ruta
+    $array = explode('/', $ruta,);
+    //validar si nos encontramos en la ruta admin
+    if ($isAdmin && (count($array) == 1 
+    || (count($array) ==2 && empty($array[1]))) 
+    && $array[0] == ADMIN) {
+        //CREAR CONTROLADOR
+        $controller = 'admin';
+        $metodo = 'login';
+    } else {
+        $indiceUrl = ($isAdmin) ? 1 : 0 ;
+        $controller = ucfirst($array[$indiceUrl]);
+        $metodo = 'index';
+    }
+    echo 'Nombre controller: ' . $controller . '<br>';
+    echo 'Nombre metodo: ' . $metodo;
 ?>
